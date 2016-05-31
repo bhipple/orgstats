e0ee45fd172706fa5fe8249aab78d2998f828cd9
Benjamin Hipple <bhipple@bloomberg.net>
2016-05-31 02:11:04 -0400

    Worked on Bloomberg github page!

---
+++ src/Main.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import GitHub
+import GitHub.Auth
+import GitHub.Endpoints.Repos
+import Turtle
+
+import Data.Maybe (fromMaybe, fromJust)
+import Data.String (fromString)
+import System.Directory (createDirectoryIfMissing, doesDirectoryExist)
+import System.Environment (getEnv, lookupEnv)
+
+import Data.Text hiding (empty)
+import qualified Data.Vector as V
+
+org = "scrp"
+outdir = "/Users/bhipple/public_html/"
+
+main :: IO ()
+main = do
+    token <- getEnv "GITHUB_TOKEN"
+    endpoint <- fromMaybe "https://api.github.com" <$> lookupEnv "GITHUB_API_ENDPOINT"
+    let auth = GitHub.EnterpriseOAuth endpoint (fromString token)
+    resp <- organizationRepos' (Just auth) (mkOrganizationName org) RepoPublicityAll
+    case resp of
+        (Left error) -> print error
+        (Right repos) -> do
+            -- Only take repos that have been initialized
+            let r' = V.filter ((>0) . fromJust . repoSize) repos
+            createDirectoryIfMissing True ".work"
+            V.mapM_ download r'
+            V.mapM_ (generateStats outdir) r'
+
+    putStrLn "Done"
+
+download :: Repo -> IO ()
+download r = do
+    let dirName = ".work/" <> toName r
+    print $ "Processing " <> dirName
+    res <- doesDirectoryExist (unpack dirName)
+    if res
+        then shells ("cd " <> dirName <> " && git pull") empty
+        else shells ("cd .work && git clone " <> repoHtmlUrl r) empty
+
+toName :: Repo -> Text
+toName = untagName . repoName
+
+generateStats :: Text -> Repo -> IO ()
+generateStats fp r = let rn = toName r
+                         outpath = fp <> "/" <> org <> "/" <> rn in
+    shells ("gitstats .work/" <> rn <> " " <> outpath) empty
